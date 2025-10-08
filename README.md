b47b36081d080589a4764523762428087156eee9
eomayski <emil@omayski.com>
2025-10-08 21:00:32 +0300

    Add Express Sever

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Workshop: Movie Magic
 SoftUni JS Back-End Workshop
+
+## Development Steps
+- [x] Initialize Project
+- [x] Add Express Server
+- [ ] Add Workshop Resources
+- [ ] Setup Handlebars
+- [ ] Render Home Page
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, Movie Magic!');
+import express from 'express';
+
+const app = express();
+
+app.get('./', (req, res) => {
+    res.send("It Works!");
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+
