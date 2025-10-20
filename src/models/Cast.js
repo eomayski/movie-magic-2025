bf22b951cd186fc2e189ff5555a500d3af733b98
eomayski <emil@omayski.com>
2025-10-20 21:11:40 +0300

    Cast partial added

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -3,16 +3,21 @@ import { Schema, model } from "mongoose";
 const castSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Cast name is required']},
+        required: [true, 'Cast name is required'],
+        minLength: [5, 'Name is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Cast name has some invalid characters']},
     age: {
         type: Number,
         required: [true, 'Cast age is required'],
-        max: 120,
-        min: 0
+        max: [120, 'Age can not be more than 120'],
+        min: [1, 'Cast must be at least 1 year old']
     },
     born: {
         type: String,
-        required: true},
+        required: true,
+        minLength: [5, 'Name is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Cast name has some invalid characters']
+    },
     imageUrl: {
         type: String,
         required: true}
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,14 +1,50 @@
 import { Schema, Types, model } from "mongoose";
 
 const movieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    title: {
+        type: String,
+        required: [true, 'Movie title is required!'],
+        minLength: [5, 'Title is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Title has some invalid characters']
+    },
+    category: {
+        type: String,
+        enum: {
+            values: ["tv-show", "animation", "movie", "documentary", "short-film"],
+            message: 'Your category is invalid!'
+        },
+        required: [true, 'Movie category is required!']
+    },
+    genre: {
+        type: String,
+        required: [true, 'Movie genre is required!'],
+        minLength: [5, 'Title is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Genre has some invalid characters']
+    },
+    director: {
+        type: String,
+        required: [true, 'Movie director is required!'],
+        minLength: [5, 'Title is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'director has some invalid characters']
+    },
+    year: {
+        type: Number,
+        min: [1900, 'Year can not be before 1900'],
+        max: [2024, 'Year can not be after 2024']
+    },
+    imageUrl: {
+        type: String,
+        match: [/^https?:\/\//]
+    },
+    rating: {
+        type: Number,
+        min: [1, 'Minimal rating is 1'],
+        max: [10, 'Maximal rating is 10']
+    },
+    description: {
+        type: String,
+        minLength: [20, 'Description must be at least 20 characters long.']
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
